66e85a3f4028ed3a4734a8ebe9d962f420b08a88
Felipe Henrique <felipehfsouza@gmail.com>
2018-06-01 17:53:52 -0300

    Ordem dos botões

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -9,12 +9,12 @@ export default class Grid extends React.Component {
     render(){
         return (
             <div className="grid">
+                <Button value="C" onClick={this.props.clear} />
                 {
                     this.props.operators.map((item, index) => 
                         <Button value={item} key={index} onClick={this.props.onClick}/>
                     )
                 }
-                <Button value="C" onClick={this.props.clear} />
                 <Button value="=" onClick={this.props.calculate} />
             </div>
         )
